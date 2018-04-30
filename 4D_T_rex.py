e92611ee33da6387e9442c39aefb084435d371c3
Paul <13591438+Breakfast-for-Pigeons@users.noreply.github.com>
2018-04-29 21:24:36 -0500

    Update 4D_T_rex.py

--- 4D_T_rex.py
+++ 4D_T_rex.py
@@ -3,10 +3,9 @@
 #                        4D Tyrannosaurus rex                          #
 ########################################################################
 # Description:                                                         #
-# This program hacks a toy dinosaur. A button is pressed to make the   #
-# dinosaur move and roar.                                              #
-# This program is also a demonstration of controlling a motor using    #
-# the gpiozero module.                                                 #
+# This program controls the motor of a toy dinosaur. A button is       #
+# pressed to make the T. rex move and roar.                            #
+#                                                                      #
 # This program is also an example of adding color to text displayed to #
 # the screen.                                                          #
 #                                                                      #
@@ -16,12 +15,11 @@
 ########################################################################
 
 ########################################################################
-#                          Import files                                #
+#                          Import modules                              #
 ########################################################################
 
 from gpiozero import Motor, Button, OutputDevice
 from time import sleep
-from signal import pause
 import pygame
 import random
 import os, sys, logging
@@ -48,12 +46,12 @@ logging.basicConfig(filename='Files/T_rex.log', filemode='w',
 ########################################################################
 #                            Functions                                 #
 ########################################################################
-'''
-This is the main function. It will wait until one of two buttons is
-pressed. One button will start the program and the other button will
-stop the program. Pressing Ctrl-C will also stop the program.
-'''
 def main():
+	"""
+	This is the main function. It will wait until one of two buttons is
+	pressed. One button will activate the T. rex and the other button 
+	will stop the program. Pressing Ctrl-C will also stop the program.
+	"""
 	
 	try:
 		logging.info("START")
@@ -92,12 +90,13 @@ def main():
 	except KeyboardInterrupt:
 		stop_the_program()
 
-'''
-The file_check function checks to see if the necessary files exist.
-If they all exist, the program will continue.
-If a file is missing, the program will print a message and exit.
-'''
 def file_check():
+	"""
+	The file_check function checks to see if the necessary files exist.
+	If they all exist, the program will continue.
+	If a file is missing, the program will print out a message to the 
+	screen and then exit.
+	"""
 	
 	file_missing_flag = 0
 	
@@ -109,7 +108,7 @@ def file_check():
 	if os.path.isfile('Files/dinosaur_facts.txt'):
 		logging.info("dinosaur_facts.txt file was found!")
 	else:
-		detail_log.error("dinosaur_facts.txt file was not found! Make sure that the dinosaur_facts.txt file exists in the Files folder.")
+		logging.error("dinosaur_facts.txt file was not found! Make sure that the dinosaur_facts.txt file exists in the Files folder.")
 		file_missing_flag = 1
 	
 	# Check to see if sound files exists
@@ -128,12 +127,13 @@ def file_check():
 		print("\033[1;31;40mErrors were encountered. Check the log in the 'Files' folder for more information.")
 		stop_the_program()
 
-'''
-The permission_check function checks to see if the user has permission
-to read the necessary files. If so, the program will continue. If not,
-messages are printed out to the screen and the program will exit.
-'''
+
 def permission_check():
+	"""
+	The permission_check function checks to see if the user has permission
+	to read the necessary files. If so, the program will continue. If not, 
+	the program will print out a message to the screen and then exit.
+	"""
 	
 	permission_flag = 0
 	
@@ -162,41 +162,50 @@ def permission_check():
 		print("\033[1;31;40mErrors were encountered. Check the log in the 'Files' folder for more information.")
 		stop_the_program()
 
-'''
-The read_file function will read the dinosaur facts file and each 
-line of the file will be an element in the fun_facts list. It will then
-return the dino_facts list to the main function.
-If the program is unable to read the file, it will display an error
-message and then exit the program.
-If the dino_facts file is empty, an error message will be displayed
-and the program will exit.
-'''
 def read_file(file_name):
+	"""
+	The read_file function has one parameter: file_name. In this program, the 
+	argument passed in will be the dinosaur_facts.txt file located in the 
+	'Files' folder. Each line of the file will be an element in the dino_facts 
+	list. It will then return the dino_facts list to the main function.
+	If the program is unable to populate the list, it will display an error
+	message and then exit the program.
+	"""
+	
 	logging.info("READING DINOSAUR_FACTS.TXT")
-	with open(file_name, "r") as f:         # open the file as read-only
-		dino_facts = f.readlines()
+	try:
+		with open(file_name, "r") as f:         # open the file as read-only
+			dino_facts = f.readlines()
+		logging.info("The dino_facts list was successfully populated.")
+	except:
+		print("\033[1;31;40mErrors were encountered. Check the log in the 'Files' folder for more information.")
+		logging.error("The dino_facts list could not be populated.")
+		stop_the_program()
 
 	return dino_facts
 
-'''
-This empty_file_check function checks to see if the file is empty. If it
-is, the program will print a message to the screen. If not, the program
-will continue.
-'''
-def empty_file_check(file_name):
+def empty_file_check(list_name):
+	"""
+	The empty_file_check function has one parameter: file_name. In this program, the 
+	argument passed in will be the dino_facts list. It will checks to see 
+	if the list is empty. If it is, the program will print a message to the 
+	screen. If it is not empty, the program will continue.
+	"""
+	
 	logging.info("EMPTY FILE CHECK")
-	if file_name == []:
+	if list_name == []:
 		logging.error("The dinosaur.txt file is empty. The program won't work.")
 		print("\033[1;31;40mErrors were encountered. Check the log in the 'Files' folder for more information.")
 		stop_the_program()
 	else:
 		logging.info("The dinosaur.txt file is not empty.(This is good. We don't want an empty file.)")
 
-'''
-The print_header function will print out the program header to the
-screen.
-'''
 def print_header():
+	"""
+	The print_header function will print out the program header to the
+	screen.
+	"""
+	
 	print("\n")
 	print("\033[1;37;40m==========================================================================================================")
 	print("\033[1;37;40m   _  _   ____    _____                                                                                   ")
@@ -208,62 +217,75 @@ def print_header():
 	print("\033[1;37;40m==========================================================================================================")                                                      
 	print("\n")
 
-'''
-The get_roar function will randomly select one of the T. rex roar
-sound files and return it and its file length to the main function.
-'''
 def get_roar():
+	"""
+	The get_roar function will randomly select one of the T. rex roar
+	sound files and then return it and its file length to the main function.
+	"""
 	
-	# The Key : Value pair is sound file name : length of file in seconds
+	# The key/value pair is sound file name : length of file in seconds
 	roars = {'Sounds/T_rex1.ogg' : 6.5 , 'Sounds/T_rex2.ogg' : 3 , 'Sounds/T_rex3.ogg' : 4 , 
 			 'Sounds/T_rex4.ogg' : 5.5 , 'Sounds/T_rex5.ogg' : 4 , 'Sounds/T_rex6.ogg' : 6 ,
 			 'Sounds/T_rex7.ogg' : 4.5 , 'Sounds/T_rex8.ogg' : 4 }
 	
 	return random.choice(list(roars.items()))
 
-'''
-The prompt_user_for_input function prompts a user to push a button.
-'''
 def prompt_user_for_input():
+	"""
+	The prompt_user_for_input function prompts a user to push a button.
+	"""
+	
 	print("\033[1;37;40mPush the \033[1;30;47mblack button\033[1;37;40m to activate the \033[1;30;47mT. Rex\033[1;37;40m.")
 	print("\033[1;37;40mPush the \033[1;31;40mred button \033[1;37;40mor press Ctrl-C to \033[1;31;40mstop \033[1;37;40mthe program.\n")
 
-'''
-The print_dinosaur_fact function prints out a random fact about 
-dinosaurs. The dino_facts file needs to be sent to this function.
-'''
+
 def print_dinosaur_fact(dino_facts):
+	"""
+	The print_dinosaur_fact function takes the dino_facts list as its
+	input. It will select a random fact and print it out.
+	"""
+	
 	print("\033[1;34;40mDINOSAUR FUN FACT:")
 	print(random.choice(dino_facts))
 
-'''
-The activate_T_rex funciton takes 2 inputs: roar and roar_length.
-This function will play the sound file and then activate the motor for
-the duration of the sound file.
-'''
-def activate_T_rex(roar, roar_length):
-	try:
-		t_rex_motor.value = 0.6       # Controls the motor speed
-	except ValueError:
-		logging.error("A bad value was specified for t_rex_motor. The value should be between 0 and 1.")
-		print("\033[1;31;40mAn error was encountered. Check the log in the 'Files' folder for more information.\n")
-		stop_the_program()
-	pygame.mixer.music.load(roar)     # Loads the sound file
-	t_rex_motor_enable.on()           # Starts the motor
-	pygame.mixer.music.play()         # Plays the sound file
-	sleep(roar_length)                # Length of sound file in seconds
-	t_rex_motor_enable.off()          # Stops the motor
 
-'''
-The release_gpio_pins function realeases the gpio pins.
-'''
+def activate_T_rex(sound, sound_length):
+	"""
+	The activate_T_rex function has two parameters: sound and sound length.
+	"sound" is the name of a sound file and "sound_length" is the length of
+	the sound file in seconds. The arguments passed to this function will be
+	one of the randomly selected roars and its length. This function will play the 
+	sound file and activate the motor for the duration of the sound file.
+	"""
+	
+	try:
+		t_rex_motor.value = 0.6        # Controls the motor speed
+	except ValueError:
+		logging.exception("ValueError: A bad value was specified for t_rex_motor. The value should be between -1 and 1.")
+		print("\033[1;31;40mA value error was encountered. Check the log in the 'Files' folder for more information.\n")
+		stop_the_program()
+	pygame.mixer.music.load(sound)     # Loads the sound file
+	t_rex_motor_enable.on()            # Starts the motor
+	pygame.mixer.music.play()          # Plays the sound file
+	sleep(sound_length)                # Length of sound file in seconds
+	t_rex_motor_enable.off()           # Stops the motor
+
 def release_gpio_pins():
+	"""
+	The release_gpio_pins function realeases the gpio pins.
+	"""
+	
 	t_rex_motor.close()
 	t_rex_motor_enable.close()
 	red_button.close()
 	black_button.close()
 
 def stop_the_program():
+	"""
+	The stop_the_program function will call the release_gpio_pins function,
+	print a message to the screen, and then exit the program.
+	"""
+	
 	release_gpio_pins()
 	print("\033[1;37;40mExiting program.\n")
 	logging.info("END")
